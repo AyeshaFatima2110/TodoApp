06b58bb65ecb7b9b93d9092e026780af63abb08f
ayesha fatima <ayeshahaneef21@gmail.com>
2024-11-18 10:52:02 +0500

    added the public folder

---
+++ public/index.js
@@ -0,0 +1,112 @@
+
+const submitBtn = document.querySelector('.submit');
+const taskInput = document.querySelector('.js-tasksInput');
+const dateInput = document.querySelector('.js-dateInput');
+const taskList = document.querySelector('.task-list');
+
+let dl = '';
+
+
+
+getAllTask();
+
+
+
+
+
+
+
+submitBtn.addEventListener('click',(event)=>{
+  event.preventDefault();
+  const taskname = taskInput.value;
+  const taskdate = dateInput.value;
+  
+  const obj = {
+    taskname,
+    taskdate
+  }
+  
+  
+  sendRequest(obj);
+  getAllTask();
+  
+  
+  
+});
+
+
+async function sendRequest(obj){
+  let response;
+  try{
+    response = await fetch('http://localhost:3000/task/submit' , {
+      method :'POST',
+      headers :{
+        "Content-Type" : 'application/json'
+      },
+      body : JSON.stringify(obj)
+    });
+    if (!response.ok) {
+      const errorMessage = await response.json(); 
+      alert(`Error : ${errorMessage.message}`);
+      return;
+    }
+    
+  }
+  catch(err){
+  }
+}
+
+
+async function getAllTask(){
+  let html = '';
+  
+  try{
+    const response = await fetch('http://localhost:3000/task');
+    const result = await response.json();
+    result.forEach((task,index)=> {
+      const date = new Date(task.taskdate);
+      const formattedDate = `${String(date.getDate()).padStart(2, '0')}/${String(date.getMonth() + 1).padStart(2, '0')}/${date.getFullYear()}`;
+
+      //const div = document.createElement('div');
+      //div.classList.add('task-styling');
+      //taskList.appendChild(div);
+
+      // const indexSpan = document.createElement('span');
+      // const nameSpan = document.createElement('span');
+      // const dateSpan = document.createElement('span');
+      // const deleteSpan = document.createElement('span');
+
+      // indexSpan.innerHTML = `${index+1}`;
+      // nameSpan.innerHTML = `${task.taskname}`;
+      // dateSpan.innerHTML = `${formattedDate}`
+      // deleteSpan.innerHTML = deleteIcon;
+
+      // deleteSpan.classList.add('delete-icon');
+
+      // div.appendChild(indexSpan);
+      // div.appendChild(nameSpan);
+      // div.appendChild(dateSpan);
+      // div.appendChild(deleteSpan);
+
+      html+= `<div class = "task-styling"> 
+              <span>${index+1}</span>
+              <span>${task.taskname}</span>
+              <span>${formattedDate}</span>
+              <span class='delete-icon'><i class="fa-solid fa-trash"></i></span>
+              </div>`
+         
+      });
+      
+      
+
+}
+  catch(err){
+    console.log(`error : ${err}`);}
+
+  taskList.innerHTML = html ;
+  dl = document.querySelectorAll('.delete-icon');
+
+}
+
+
+
